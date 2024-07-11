6ccdacb469a3bf8ee67b8a7013504912d23556c5
splintergu <splintergu@gmail.com>
2024-07-11 20:06:42 -0300

    - fix same message if no tape selected when press F6

--- src/OSDMain.cpp
+++ src/OSDMain.cpp
@@ -515,10 +515,14 @@ void OSD::do_OSD(fabgl::VirtualKey KeytoESP, bool CTRL) {
         }
         else if (KeytoESP == fabgl::VK_F6) {
             // Start / Stop .tap reproduction
-            if (Tape::tapeStatus == TAPE_STOPPED)
-                Tape::Play();
-            else
-                Tape::Stop();
+            if (Tape::tapeFileName=="none") {
+                OSD::osdCenteredMsg(OSD_TAPE_SELECT_ERR[Config::lang], LEVEL_WARN);
+            } else {
+                if (Tape::tapeStatus == TAPE_STOPPED)
+                    Tape::Play();
+                else
+                    Tape::Stop();
+            }
             click();
         }
         else if (KeytoESP == fabgl::VK_F7) {
